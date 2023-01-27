016f9ab0ba5f886b206c87f690ad40e9c754c206
Roman <R.rybkin94@gmail.com>
2023-01-27 14:15:19 +0400

    first commit

---
+++ src/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from './Header.module.css'
+
+
+export const Header = () => {
+    return (
+        <div className={styled.header}>
+
+        </div>
+    );
+};
+

35fc3a0c332ff7dc478139405df89851aeec98e5
Roman <R.rybkin94@gmail.com>
2023-03-12 13:45:13 +0400

    Added Main1

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -5,7 +5,11 @@ import style from './Main.module.scss'
 export const Main = () => {
     return (
         <div className={style.main}>
+            <div className={style.container}>
+                <div className={style.containerCenter}>
 
+                </div>
+            </div>
         </div>
     );
 };

ea1213f2e83b72e9835a4357b2758c9204914fd7
Roman <R.rybkin94@gmail.com>
2023-03-12 11:34:56 +0400

    Added Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./header/Header";
-import {Main} from "./main/Main";
+import {AboutMe} from "./aboutMe/AboutMe";
 import {Skills} from "./skills/Skills";
 import {Projects} from "./project/Projects";
 import {Contacts} from "./сontacts/Contacts";
 import {Footer} from "./footer/Footer";
+import {Main} from "./main/Main";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Main/>
+            <AboutMe/>
             <Skills/>
             <Projects/>
             <Contacts/>
---
+++ src/aboutMe/AboutMe.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from './AboutMe.module.scss'
+import styleContainer from './../common/styles/Container.module.css'
+import photoProfile from '../common/assets/photoProfile/Profile.jpg'
+import Title from "../common/componets/title/Title";
+import {Address} from "./adress/Address";
+
+
+export const AboutMe = () => {
+    const projectsContainer = styleContainer.container + " " + styled.mainContainer
+    return (
+        <div className={styled.mainBlock}>
+
+            <div className={projectsContainer}>
+                <Title description={'Get to know me'} title={'About Me'}/>
+                <div className={styled.main}>
+                    <div className={styled.photo}>
+                        <img src={photoProfile} alt="Profile"/>
+                    </div>
+                    <div className={styled.profile}>
+                        <h2>Who am i?</h2>
+                        <h6>I'm Roman Rybkin, a visual UX/UI Designer and Web Developer</h6>
+                        <p className={styled.text}>I am a freelancer based in the United Kingdom
+                            and i have been building noteworthy UX/UI designs and websites for years,
+                            which comply with the latest design trends.
+                            I help convert a vision and an idea into meaningful and useful products.
+                            Having a sharp eye for product evolution helps me prioritize tasks,
+                            iterate fast and deliver faster.</p>
+                        <Address/>
+                        <div className={styled.cvAndSocialNetworks}>
+                            <a href="" role={'button'}>Download CV</a>
+                            <ul className={styled.followSocialNetwork}>
+                                <li className={styled.item}>s</li>
+                                <li className={styled.item}>s</li>
+                                <li className={styled.item}>s</li>
+                                <li className={styled.item}>s</li>
+                                <li className={styled.item}>s</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+

9000a5a354e9c405b6e6adddd8a2657ac50fa7b2
Roman <R.rybkin94@gmail.com>
2023-05-03 21:52:43 +0400

    update api

--- src/api/portfolio-api.ts
+++ src/api/portfolio-api.ts
@@ -1,20 +1,11 @@
 import axios from "axios";
-import {Dispatch} from "redux";
 
-// const instance = axios.create({
-//     baseURL: "http://localhost:3010/sendMessage"
-// })
 
-// export const portfolioApi = {
-//     sendMessageGmail(data: FormType){
-//         return axios.post('http://localhost:5000/sendMessage', data)
-//     }
-// }
 export const portfolioApi = {
     sendMessageGmail(data: FormType){
-        return axios.post('https://potfolio-app-zeta.vercel.app/message', data, {headers: {
+        return axios.post('https://backend-portfolio-production-e60f.up.railway.app/message', data, {headers: {
                 'Content-Type': 'application/json'
-            }, withCredentials: true})
+            }})
     }
 }
 

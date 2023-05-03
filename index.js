051509b47ab277fe7e74f02316b04d5f901629b7
Roman <R.rybkin94@gmail.com>
2023-05-03 17:56:41 +0400

    update api

--- index.js
+++ index.js
@@ -37,7 +37,15 @@ app.get('/', function (req, res) {
 })
 
 app.post('/sendMessage', async function (req, res) {
-
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     let {textName, email, textSubject, message} = req.body
     console.log('textName c index.js',textName)
     let info = await transporter.sendMail({
@@ -72,27 +80,3 @@ app.post('/sendMessage', async function (req, res) {
 app.listen(port, function () {
     console.log('Example app listening on port 5000')
 })
-
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
-
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-}
-
-module.exports = allowCors(handler)

5d3e4faedc5695321f83d6639eda03735eef7d5d
Roman <R.rybkin94@gmail.com>
2023-05-03 15:27:12 +0400

    update api

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const nodemailer = require("nodemailer");
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-const router = express.Router();
+// const router = express.Router();
 
 
 
 const app = express();
-
+let port = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -68,7 +68,7 @@ app.post('/sendMessage', async function (req, res) {
     res.send(req.body)
 })
 
-let port = process.env.PORT || 5000
+
 
 app.listen(port, function () {
     console.log('Example app listening on port 5000')

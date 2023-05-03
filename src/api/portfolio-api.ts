af14d5243dad0919ae46c0c04a525a201205ebf5
Roman <R.rybkin94@gmail.com>
2023-05-03 14:51:24 +0400

    vercel

--- src/api/portfolio-api.ts
+++ src/api/portfolio-api.ts
@@ -5,12 +5,16 @@ import {Dispatch} from "redux";
 //     baseURL: "http://localhost:3010/sendMessage"
 // })
 
+// export const portfolioApi = {
+//     sendMessageGmail(data: FormType){
+//         return axios.post('http://localhost:3009/sendMessage', data)
+//     }
+// }
 export const portfolioApi = {
     sendMessageGmail(data: FormType){
-        return axios.post('http://localhost:3009/sendMessage', data)
+        return axios.post('https://potfolio-app-zeta.vercel.app/sendMessage', data)
     }
 }
-
 
 export type FormType = {
     textName: string

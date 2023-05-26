172aad1bcd53cd619c1c35f23f1222a90b16d1b6
Roman <R.rybkin94@gmail.com>
2023-05-26 14:36:39 +0400

    refactoring cv

---
+++ src/common/componets/WaveAnimation/WaveAnimation.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// @ts-ignore
+import FluidAnimation from 'react-fluid-animation'
+
+type WaveAnimationPropsType={
+    className?: string
+}
+
+export const WaveAnimation = (props: WaveAnimationPropsType) => {
+    return (
+        <>
+            <FluidAnimation
+                // style={{ height: '100vh' }}
+                className={props.className}
+            />
+        </>
+    );
+};
+
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -3,7 +3,11 @@ import style from './Main.module.scss'
 import {IconSocialNetwork} from "../common/componets/icon/IconSocialNetwork/IconSocialNetworkType";
 import {ReactTypingEffectText} from "../common/componets/typingText/ReactTypingEffect";
 import React from 'react'
-
+import {WaveAnimation} from "../common/componets/WaveAnimation/WaveAnimation";
+import styleContainer from "../common/styles/Container.module.scss";
+import styled from "../project/Projects.module.scss";
+import {Header} from "../header/Header";
+// import FluidAnimation from 'react-fluid-animation'
 
 
 
@@ -20,18 +24,21 @@ export const Main = () => {
     }
 
     const textTyping = ["React developer.", "Frontend developer."]
+    // const projectsContainer = styleContainer.container + " " + styled.projectContainer
 
     return (
-        <div className={style.main}>
-            <div className={style.container}>
-                <div className={style.containerCenter}>
-                    <div className={style.textCenter}>
-                        <h1 className={style.name}>
-                            Roman Moisidi
-                        </h1>
-                        <h4 className={style.headLine}>
-                            I’m a
-                            <span className={style.singleHeadLine}>
+        <>
+            <WaveAnimation className={style.fluidAnimation}/>
+            <div className={style.main}>
+                <div className={style.container}>
+                    <div className={style.containerCenter}>
+                        <div className={style.textCenter}>
+                            <h1 className={style.name}>
+                                Roman Moisidi
+                            </h1>
+                            <h4 className={style.headLine}>
+                                I’m a
+                                <span className={style.singleHeadLine}>
                                 <ReactTypingEffectText
                                     text={textTyping}
                                     speed={200}
@@ -40,16 +47,20 @@ export const Main = () => {
                                     typingDelay={500}
                                 />
                             </span>
-                        </h4>
+                            </h4>
+                        </div>
+
+                        {/*<div className={style.fixedBlock}>*/}
+                        {/*    <IconSocialNetwork style={iconStyle} styleLi={styleLi}/>*/}
+                        {/*</div>*/}
+
                     </div>
 
-                    <div className={style.fixedBlock}>
-                        <IconSocialNetwork style={iconStyle} styleLi={styleLi}/>
-                    </div>
+
                 </div>
-
             </div>
-        </div>
+        </>
+
     );
 };
 
